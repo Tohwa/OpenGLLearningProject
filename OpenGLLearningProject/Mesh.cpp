1414a0a0b11f485eecfdc71ac79dfb808a375c4c
Tohwa | Sascha <Tohwa.gaming@gmail.com>
2024-05-16 06:10:05 +0200

    optimization failed miserably :D |

--- OpenGLLearningProject/Mesh.cpp
+++ OpenGLLearningProject/Mesh.cpp
@@ -1,86 +1,19 @@
 #include "Mesh.h"
 #include "GameObject.h"
 
-//Mesh::Mesh()
-//{
-//}
-
 Mesh::Mesh(Transform& _transform)
 {
 	position = _transform.position;
 }
 
-//void Mesh::Initialize(SShader* _shader, std::string _path, bool _canOrbit)
-//{
-//	/*canOrbit = _canOrbit;
-//	vertices = _vertices;
-//
-//	indices = _indices;*/
-//	//ObjLoader loader(_path);
-//	ObjLoader loader("../Obj/IcoSphere.obj");
-//
-//	loader.ReadFile();
-//
-//	vertices = loader.finalVertices;
-//	indices = loader.GenerateIndices(loader.out_vertices, loader.out_normals, loader.out_uvs, loader.indices);
-//
-//	shader = _shader;
-//	CreateBuffers();
-//
-//	m_modelID = glGetUniformLocation(shader->id, "model");
-//	m_viewID = glGetUniformLocation(shader->id, "view");
-//	m_projID = glGetUniformLocation(shader->id, "projection");
-//	m_normID = glGetUniformLocation(shader->id, "normal");
-//	m_camPosID = glGetUniformLocation(shader->id, "cameraPosition");
-//
-//	model = glm::translate(glm::mat4(1.0), position);
-//	normal = glm::inverse(glm::mat3(model));
-//
-//	
-//}
-
-//void Mesh::Update()
-//{
-//	//Rotate(0.01f, glm::vec3(0.0f, 1.0f, 0.0f));
-//	//Rotate(0.03f, glm::vec3(0.1f, 0.0f, 0.0f));
-//	//Rotate(0.01f, glm::vec3(0.0f, 0.0f, 1.0f));
-//
-//	//if (canOrbit)
-//	//{
-//	//	angle += 0.001f; // Ändere den Winkel für die Rotation
-//	//	if (angle > 2 * glm::pi<float>())
-//	//	{
-//	//		angle -= 2 * glm::pi<float>(); // Winkel nicht über 360 Grad halten
-//	//	}
-//
-//	//	Orbit(position.x, glm::vec3(0.0f, 1.0f, 0.0f));
-//	//}
-//}
-
-//void Mesh::Draw(const Camera& _camera)
-//{
-//	//shader->Use();
-//
-//	glUniformMatrix4fv(m_modelID, 1, GL_FALSE, &model[0][0]);
-//	glUniformMatrix4fv(m_viewID, 1, GL_FALSE, &_camera.view[0][0]);
-//	glUniformMatrix4fv(m_projID, 1, GL_FALSE, &_camera.projection[0][0]);
-//	glUniformMatrix3fv(m_normID, 1, GL_TRUE, &normal[0][0]);
-//	glUniform3fv(m_camPosID, 1, &(_camera.position.x));
-//
-//	glBindVertexArray(m_vao);
-//	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-//}
-
-
-
 const int Mesh::Initialize(GameObject& _gameObject)
 {
 
-	ObjLoader loader("../Obj/IcoSphere.obj");
+	ObjLoader loader("../Obj/planet.obj");
 
 	loader.ReadFile();
 
-	vertices = loader.GenerateVertices();;
+	vertices = loader.GenerateVertices();
 	indices = loader.indices;
 
 	shader = _gameObject.m_shader;
--- OpenGLLearningProject/ObjLoader.cpp
+++ OpenGLLearningProject/ObjLoader.cpp
@@ -2,132 +2,21 @@
 
 ObjLoader::ObjLoader(const std::string& filePath) : path(filePath){}
 
-float ObjLoader::randomFloat01()
-{
-    // Erstelle einen Zufallszahlengenerator
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
-
-    // Generiere und gib eine zufällige Float-Zahl zurück
-    return dis(gen);
-}
-
-//void ObjLoader::ReadFile() {
-//    std::ifstream ObjFile(path);
-//
-//    if (!ObjFile.is_open()) {       
-//        std::cout << "Failed to read file!" << std::endl;
-//        return;
-//    }
-//
-//    char lineHeader[128];
-//
-//    while (ObjFile >> lineHeader) {
-//        if (strcmp(lineHeader, "v") == 0) {
-//            glm::vec3 vertex;
-//            ObjFile >> vertex.x >> vertex.y >> vertex.z;
-//            temp_vertices.push_back(vertex);
-//        }
-//        else if (strcmp(lineHeader, "vt") == 0) {
-//            glm::vec2 uv;
-//            ObjFile >> uv.x >> uv.y;
-//            temp_uvs.push_back(uv);
-//        }
-//        else if (strcmp(lineHeader, "vn") == 0) {
-//            glm::vec3 normal;
-//            ObjFile >> normal.x >> normal.y >> normal.z;
-//            temp_normals.push_back(normal);
-//        }
-//        else if (strcmp(lineHeader, "f") == 0) {
-//            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-//            for (int i = 0; i < 3; ++i) {
-//                ObjFile >> vertexIndex[i];
-//                ObjFile.ignore(1); // Ignore '/'
-//                ObjFile >> uvIndex[i];
-//                ObjFile.ignore(1); // Ignore '/'
-//                ObjFile >> normalIndex[i];
-//            }
-//
-//            for (int i = 0; i < 3; ++i) {
-//                vertexIndices.push_back(vertexIndex[i]);
-//                uvIndices.push_back(uvIndex[i]);
-//                normalIndices.push_back(normalIndex[i]);
-//            }
-//        }
-//        else {
-//            // Ignore other types of lines
-//            std::string dummyLine;
-//            std::getline(ObjFile, dummyLine);
-//        }
-//    }
-//
-//
-//    for (int i = 0; i < vertexIndices.size(); i++) {
-//        unsigned int vertexIndex = vertexIndices[i];
-//        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
-//        out_vertices.push_back(vertex);
-//    }
-//
-//    for (int i = 0; i < normalIndices.size(); i++) {
-//        unsigned int normalIndex = normalIndices[i];
-//        glm::vec3 normal = temp_normals[normalIndex - 1];
-//        out_normals.push_back(normal);
-//    }
-//
-//    for (int i = 0; i < uvIndices.size(); i++) {
-//        unsigned int uvIndex = uvIndices[i];
-//        glm::vec2 UV = temp_uvs[uvIndex - 1];
-//        out_uvs.push_back(UV);
-//    }
-//
-//    for (int i = 0; i < vertexIndices.size(); i++) {
-//        SVertex vertex{ out_vertices[i], glm::vec4{randomFloat01(),randomFloat01(),randomFloat01(),1.0}, out_normals[i], out_uvs[i]};
-//        finalVertices.push_back(vertex);
-//    }
-//
-//    ObjFile.close();
-//
-//}
-//
-//
-//std::vector<unsigned int> ObjLoader::GenerateIndices(std::vector<glm::vec3> _vertices, std::vector<glm::vec3> _normals, std::vector<glm::vec2> _uvs, std::vector<unsigned int> _indices)
+//float ObjLoader::randomFloat01()
 //{
-//    // Leere die vorhandenen Indizes, da wir sie neu generieren werden
-//    _indices.clear();
+//    // Erstelle einen Zufallszahlengenerator
+//    std::random_device rd;
+//    std::mt19937 gen(rd());
+//    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 //
-//    // Durchlaufe alle Vertices
-//    for (size_t i = 0; i < _vertices.size(); ++i) {
-//        // Erzeuge einen eindeutigen Schlüssel für Position, Normale und Texturkoordinaten
-//        std::string key = std::to_string(i);
-//        if (!_normals.empty()) {
-//            key += "_" + std::to_string(i); // Füge den Index für die Normale hinzu
-//        }
-//        if (!_uvs.empty()) {
-//            key += "_" + std::to_string(i); // Füge den Index für die Texturkoordinate hinzu
-//        }
-//
-//        // Überprüfe, ob dieser Schlüssel bereits existiert
-//        auto it = IndexMap.find(key);
-//        if (it == IndexMap.end()) {
-//            // Wenn nicht, füge diesen Schlüssel hinzu und füge den Index hinzu
-//            unsigned int newIndex = static_cast<unsigned int>(_indices.size()); // Neuer Index ist die aktuelle Größe des Indexvektors
-//            _indices.push_back(newIndex); // Füge den neuen Index zum Indexvektor hinzu
-//            IndexMap[key] = newIndex; // Speichere den neuen Index in IndexMap
-//        }
-//        else {
-//            // Wenn ja, verwende den vorhandenen Index
-//            _indices.push_back(it->second); // Füge den vorhandenen Index zum Indexvektor hinzu
-//        }
-//    }
-//
-//    return _indices; // Gib die generierten Indizes zurück
+//    // Generiere und gib eine zufällige Float-Zahl zurück
+//    return dis(gen);
 //}
 
- void ObjLoader::ReadFile() {
+void ObjLoader::ReadFile() {
     std::ifstream ObjFile(path);
 
-    if (!ObjFile.is_open()) {       
+    if (!ObjFile.is_open()) {
         std::cout << "Failed to read file!" << std::endl;
         return;
     }
@@ -193,6 +82,7 @@ std::vector<SVertex> ObjLoader::GenerateVertices() {
 
     return vertices;
 }
+
 
 std::vector<unsigned int> ObjLoader::GenerateIndices(const std::vector<unsigned int>& vertexIndices, 
                                                      const std::vector<unsigned int>& normalIndices, 
