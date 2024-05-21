060815410e27555c16d13f67f50d6d53dbdcef08
Sascha <Tohwa.gaming@gmail.com>
2024-05-21 10:53:10 +0200

    +

--- OpenGLLearningProject/Viewport.h
+++ OpenGLLearningProject/Viewport.h
@@ -24,10 +24,10 @@ private:
 	const int M_I_GLFW_VERSION_MAJOR = 3;
 	const int M_I_GLFW_VERSION_MINOR = 3;
 
-	const int M_I_WINDOW_WIDTH = 1920;
-	const int M_I_WINDOW_HEIGHT = 1080;
+	const int M_I_WINDOW_WIDTH = 1280;
+	const int M_I_WINDOW_HEIGHT = 720;
 
-	const char* M_S_TITLE = "UE7 ist toll!";
+	const char* M_S_TITLE = "Solar System for dummmies";
 };
 
 #endif
